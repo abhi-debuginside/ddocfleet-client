c05f6bf5264a8b7af58ad2d7f447c84bd496c027
abhi-debuginside <abhijth@hotmail.com>
2019-10-31 19:07:31 +0530

    Project file structure rearranged  - 6 files added, 5 deleted

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Hello from "./Hello";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React"
+    };
+  }
+
+  render() {
+    return (
+      // https://www.wix.com/website-template/view/html/653
+      // https://material-ui.com/getting-started/usage/
+      <div>
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen11 :)
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
